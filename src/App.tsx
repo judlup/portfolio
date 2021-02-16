0314b0e70714260c64679348a7b862ead6e76ba8
Judlup <judlup@gmail.com>
2021-02-15 22:10:04 -0500

    15-02-2021 | Footer added | Judlup

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 import "./App.css";
+
 import Header from "./Components/Header/Header";
+import Home from "./Pages/Home/Home";
+import Footer from "./Pages/Footer/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-    </div>
+    <Router>
+      <div>
+        <Header />
+        <Switch>
+          {/* <Route path="/login" component={Login} /> */}
+          <Route exac path="/" component={Home} />
+        </Switch>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Pages/Footer/Footer.tsx
@@ -0,0 +1,76 @@
+import React from "react";
+import "./Footer.scss";
+
+import { Avatar, Button } from "@material-ui/core";
+import {
+  FaLinkedinIn,
+  FaTwitter,
+  FaYoutube,
+  FaInstagram,
+  FaGithub,
+} from "react-icons/fa";
+
+function Footer() {
+  return (
+    <>
+      <div className="Footer">
+        <div className="avatar-footer-cont">
+          <Avatar
+            className="avatar-footer"
+            src="https://pbs.twimg.com/profile_images/1229977160783482880/2PC2_PUH_400x400.jpg"
+          ></Avatar>
+        </div>
+        <div className="footer-info">
+          <h3>Julián Darío Luna Patiño</h3>
+          <div>
+            <small>Software developer with 9 years of experience.</small>
+          </div>
+          <div>
+            <small>IoT Developer with 11 years of experience.</small>
+          </div>
+          <div>
+            <small>+15 projects made</small>
+          </div>
+          <div>
+            <small>+25 people in charge</small>
+          </div>
+        </div>
+        <div className="footer-contact">
+          <h3>Contact</h3>
+          <div>
+            <small>
+              <a href="whatsapp">+57 3175769114</a>
+            </small>
+          </div>
+          <div>
+            <small>
+              <a href="mailto:im@judlup.dev">im@judlup.dev</a>
+            </small>
+          </div>
+        </div>
+        <div className="footer-cta">
+          <div>
+            <Button variant="outlined" color="default">
+              Hire me!
+            </Button>
+          </div>
+          <div>
+            <Button variant="outlined" color="default">
+              Invite me to a coffee
+            </Button>
+          </div>
+          <div className="footer-social">
+            <FaLinkedinIn className="icons" />
+            <FaTwitter className="icons" />
+            <FaYoutube className="icons" />
+            <FaInstagram className="icons" />
+            <FaGithub className="icons" />
+          </div>
+        </div>
+      </div>
+      <div className="footer-copyright">Copyright Judlup 2021</div>
+    </>
+  );
+}
+
+export default Footer;
