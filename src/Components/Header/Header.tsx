46bc5695ab3c2e6a9b3464dc107446bee97c5b83
Judlup <judlup@gmail.com>
2021-02-15 19:32:50 -0500

    15-02-2021 | Header fixed | Judlup

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -6,28 +6,34 @@ const Header = () => {
     <div className="Header">
       <AppBar position="static" className="appBar">
         <Toolbar>
-          <Avatar src="https://pbs.twimg.com/profile_images/1229977160783482880/2PC2_PUH_400x400.jpg"></Avatar>
-          <Typography variant="h6">{" <Judlup />"}</Typography>
-          <Typography>
-            <Link className="header-navbar-links" href="#">
-              Experience
-            </Link>
-            <Link className="header-navbar-links" href="#">
-              Projects
-            </Link>
-            <Link className="header-navbar-links" href="#">
-              Recognitions
-            </Link>
-            <Link className="header-navbar-links" href="#">
-              Education
-            </Link>
-            <Link className="header-navbar-links" href="#">
-              Services
-            </Link>
-            <Link className="header-navbar-links" href="#">
-              Contact
-            </Link>
-          </Typography>
+          <div className="header-left">
+            <Avatar src="https://pbs.twimg.com/profile_images/1229977160783482880/2PC2_PUH_400x400.jpg"></Avatar>
+            <Typography variant="h6">
+              Judlup <small>.dev</small>
+            </Typography>
+          </div>
+          <div className="header-right">
+            <Typography>
+              <Link className="header-navbar-links" href="#">
+                Experience
+              </Link>
+              <Link className="header-navbar-links" href="#">
+                Projects
+              </Link>
+              <Link className="header-navbar-links" href="#">
+                Recognitions
+              </Link>
+              <Link className="header-navbar-links" href="#">
+                Education
+              </Link>
+              <Link className="header-navbar-links" href="#">
+                Services
+              </Link>
+              <Link className="header-navbar-links" href="#">
+                Contact
+              </Link>
+            </Typography>
+          </div>
         </Toolbar>
       </AppBar>
     </div>
