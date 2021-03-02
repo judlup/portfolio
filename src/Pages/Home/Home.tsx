5c58f194e154659b408d4f56f77843a93ffa56b1
Judlup <judlup@gmail.com>
2021-03-02 00:04:40 -0500

    02-03-2021 | About section | Judlup

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -11,6 +11,7 @@ import {
   FaStackOverflow,
   FaEnvelope,
   FaWhatsapp,
+  FaAddressBook,
 } from "react-icons/fa";
 
 function Home() {
@@ -83,21 +84,49 @@ function Home() {
           </Grid>
         </Grid>
       </Grid>
-      {/* <Grid container spacing={3} id="card">
-        <div className="card-paper">
-          <Grid item xs={12}>
-            <Avatar
-              className="avatar-card"
-              src="https://pbs.twimg.com/profile_images/1229977160783482880/2PC2_PUH_400x400.jpg"
-            ></Avatar>
-          </Grid>
-          <Grid item xs={6}>
-            <h1>Julián Darío Luna Patiño</h1>
-            <small>Software Architect & IoT Developer</small>
-            <span>Telecommunications Enginner </span>
-          </Grid>
-        </div>
-      </Grid> */}
+
+      <Grid container className="about-me">
+        <Grid item xs={6} className="left-side-about-me">
+          <div className="left-side-overlay"></div>
+        </Grid>
+        <Grid item xs={6} className="right-side-about-me">
+          <Typography variant="h2" className="title-about-me">
+            Hi, it's about me
+          </Typography>
+          <div className="content-container">
+            <Typography variant="body2" className="content-about-me">
+              I’m an organized person who meets the tasks and requirements that
+              I am required, capable of leading and solving problems,
+              responsible and compliant of homework, with great facility to
+              absorb theoretical knowledge and practical (autodidact), I have
+              facilities for communication and internal customer service and
+              external, I easily join a work team and I am passionate about
+              leadership and challenges, lover of my little daughter Nikol
+              Daniela.
+            </Typography>
+            <Typography variant="body2" className="content-about-me">
+              I'm a entrepreneur person, at 2013 I started Domoteco about
+              development of home automation oriented to internet of things, at
+              2013 I won a seed capital from SENA Fondo Emprender, at 2018 this
+              capital seed was condoned, with Domoteco I developed software,
+              hardware, communication protocol, app mobile, prototypes for
+              clients as Pelikan (Mexico), for all It I have expience leading
+              teams, innovating and generating new solutions for improve and get
+              new horizonts to improve day to day my work.
+            </Typography>
+            <div className="button-projects-container">
+              <Button
+                variant="contained"
+                color="primary"
+                className="button-projects-about-me"
+                startIcon={<FaAddressBook />}
+              >
+                Projects
+              </Button>
+            </div>
+          </div>
+        </Grid>
+      </Grid>
     </div>
   );
 }
