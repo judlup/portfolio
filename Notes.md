f6ad4798b06a25274d93ba0312253dc5881609b2
Judlup <judlup@gmail.com>
2021-03-13 16:57:45 -0500

    13-03-2021 | Phrase section | Judlup

--- Notes.md
+++ Notes.md
@@ -1,3 +1,15 @@
 # Fix node-sass error
 
 https://stackoverflow.com/questions/64625050/error-node-sass-version-5-0-0-is-incompatible-with-4-0-0
+
+# React skill bars
+
+https://www.npmjs.com/package/react-skillbars
+
+# Image optimizer
+
+https://tinypng.com/
+
+# Shape wave divider
+
+https://www.shapedivider.app/
--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,5 +1,5 @@
-import React from "react";
 import "./Home.scss";
+import Wave from "./Wave";
 import { Grid, Typography, Avatar, Button } from "@material-ui/core";
 import {
   FaCloudDownloadAlt,
@@ -277,19 +277,9 @@ function Home() {
           </div>
         </Grid>
       </Grid>
-      <div className="custom-shape-divider-bottom">
-        <svg
-          data-name="Layer 1"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 1200 120"
-          preserveAspectRatio="none"
-        >
-          <path
-            d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
-            className="shape-fill"
-          ></path>
-        </svg>
-      </div>
+
+      <Wave colorTop="#4c99af" colorBottom="#1687a7" />
+
       <Grid container className="skills">
         <div className="bg-overlay"></div>
         <Grid item xs={12} className="skills-container">
@@ -312,6 +302,32 @@ function Home() {
               <SkillBar skills={SKILLS3} colors={colors} height={35} />
             </Grid>
           </Grid>
+        </Grid>
+      </Grid>
+
+      <Wave colorTop="#1687a7" colorBottom="#4c99af" />
+
+      <Grid container className="phrase">
+        <Grid item xs={12} className="phrase-container">
+          <div className="phrase-segment">
+            <div className="phrase-border">
+              <span className="phrase-text">
+                In each project I give my knowledge and experience so that the
+                results are as expected.
+              </span>
+            </div>
+            <div className="phrase-button-container">
+              <Button
+                variant="contained"
+                color="primary"
+                className="phrase-button"
+                startIcon={<FaAddressBook />}
+              >
+                Projects in which I have participated
+              </Button>
+            </div>
+          </div>
+          <div className="phrase-overlay"></div>
         </Grid>
       </Grid>
     </div>
