c9fa40ef73b02fbd135bdd5c35c8f2969d6584f2
Judlup <judlup@gmail.com>
2021-03-21 15:03:40 -0500

    21-03-2021 | Testimonial section | Judlup

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -7,6 +7,7 @@ import {
   Card,
   CardContent,
   Typography,
+  Link,
 } from "@material-ui/core";
 import {
   FaCloudDownloadAlt,
@@ -19,9 +20,15 @@ import {
   FaEnvelope,
   FaWhatsapp,
   FaAddressBook,
+  FaLinkedin,
 } from "react-icons/fa";
 import SkillBar from "react-skillbars";
 import edgarAcosta from "../../Assets/images/home/edgar-acosta.jpg";
+import ximenaHoffman from "../../Assets/images/home/ximena-hoffman.jpg";
+import jonHall from "../../Assets/images/home/jon-hall.jpg";
+import juanHenao from "../../Assets/images/home/juan-henao.jpg";
+import martinRodriguez from "../../Assets/images/home/martin-rodriguez.jpg";
+import carlosNieto from "../../Assets/images/home/carlos-nieto.jpg";
 
 const SKILLS = [
   {
@@ -176,12 +183,12 @@ const colors = {
 const testimonials = [
   {
     id: 1,
-    img: "",
+    img: ximenaHoffman,
     name: "Ximena Hoffman",
     title: "HR & Administration Manager",
     company: "NEC de Colombia",
     description:
-      "Tuve la oportunidad de trabajar con Julian en NEC y siempre mostró ser un líder natural, con una excelente actitud de servicio en todo momento. Es el tipo de profesionales que ayudan a construir en equipo y encontrar diversas formas de conseguir los resultados necesarios.",
+      "I had the opportunity to work with Julian at NEC and he always showed himself to be a natural leader, with an excellent service attitude at all times. He is the type of professional who helps to build as a team and find different ways to achieve the necessary results.",
     date: "20/Jan/2021",
   },
   {
@@ -194,6 +201,46 @@ const testimonials = [
       "I had the good opportunity to get to know Julian at his period working for NEC. His work was of very positive and opportune impact for the projects my team was developing.I totally recommend Julian as an excellent resource for different kind of projects. Julie is a very creative person with a positive and 'Can Do' attitude all the time.",
     date: "26/Feb/2021",
   },
+  {
+    id: 3,
+    img: jonHall,
+    name: 'Jon "Maddong" Hall',
+    title: "Board Chair",
+    company: "Linux Professional Institute",
+    description:
+      "I have known Julian since he was in high school. He created a home automation project where he could control a whole model house from his android tablet, and it was extremely impressive. I gave him a Raspberry Pi to work into his model, which he did in a few weeks.",
+    date: "19/Jan/2021",
+  },
+  {
+    id: 4,
+    img: juanHenao,
+    name: "Juan Diego Henao",
+    title: "Junior Developer",
+    company: "Everis",
+    description:
+      "Julian Dario was a very important person for my work development, showing excellent leadership and broad knowledge in his area. Without much help from others, he was able to deal with all the obstacles that stood in his way, with great organization and quality. He is a team person, who is always predisposed to help and who, hardly, has a bad answer for others.",
+    date: "18/Jan/2021",
+  },
+  {
+    id: 5,
+    img: martinRodriguez,
+    name: "Martín Rodríguez",
+    title: "Senior Fullstack Developer",
+    company: "",
+    description:
+      "Julián has all the skills to lead development teams, a very responsible and attentive person, both with the work team and with the client.",
+    date: "17/Jan/2021",
+  },
+  // {
+  //   id: 6,
+  //   img: carlosNieto,
+  //   name: "Carlos Nieto",
+  //   title: "Software Developer",
+  //   company: "NEC de Colombia",
+  //   description:
+  //     "Julián Luna is an excellent development leader, with excellent knowledge bases, organized, charismatic, proactive and very self-teaching, it was a lot of learning to work with him.",
+  //   date: "18/Jan/2021",
+  // },
 ];
 
 function Home() {
@@ -273,11 +320,11 @@ function Home() {
         </Grid>
       </Grid>
 
-      {/* <Grid container className="about-me">
-        <Grid item xs={6} className="left-side-about-me">
+      <Grid container className="about-me">
+        <Grid item xs={12} md={6} className="left-side-about-me">
           <div className="left-side-overlay"></div>
         </Grid>
-        <Grid item xs={6} className="right-side-about-me">
+        <Grid item xs={12} md={6} className="right-side-about-me">
           <Typography variant="h2" className="title-about-me">
             Hi, it's about me
           </Typography>
@@ -319,18 +366,17 @@ function Home() {
       <Wave colorTop="#4c99af" colorBottom="#1687a7" />
 
       <Grid container className="skills">
-        <div className="bg-overlay"></div>
-        <Grid item xs={12} className="skills-container">
+        <Grid item xs={6} md={12} className="skills-container">
           <Grid container>
-            <Grid item xs={10} className="title-container-skills">
+            <Grid item xs={12} md={10} className="title-container-skills">
               <span className="title-skills">My Skills</span>
             </Grid>
-            <Grid item xs={2} className="button-container-skills">
+            <Grid item xs={6} md={2} className="button-container-skills">
               <Button className="button-skills">My Experience</Button>
             </Grid>
           </Grid>
           <Grid container>
-            <Grid item xs={12} sm={3} className="skills-body">
+            {/* <Grid item xs={12} sm={3} className="skills-body">
               <SkillBar skills={SKILLS} colors={colors} height={35} />
             </Grid>
             <Grid item xs={12} sm={3} className="skills-body">
@@ -338,7 +384,7 @@ function Home() {
             </Grid>
             <Grid item xs={12} sm={3} className="skills-body">
               <SkillBar skills={SKILLS3} colors={colors} height={35} />
-            </Grid>
+            </Grid> */}
           </Grid>
         </Grid>
       </Grid>
@@ -346,7 +392,7 @@ function Home() {
       <Wave colorTop="#1687a7" colorBottom="#4c99af" />
 
       <Grid container className="phrase">
-        <Grid item xs={12} className="phrase-container">
+        <Grid item xs={12} md={12} className="phrase-container">
           <div className="phrase-segment">
             <div className="phrase-border">
               <span className="phrase-text">
@@ -372,7 +418,7 @@ function Home() {
       <Wave colorTop="#4c99af" colorBottom="#1687a7" />
 
       <Grid container className="testimonial">
-        <Grid item xs={12} className="testimonial-header">
+        <Grid item xs={12} md={12} className="testimonial-header">
           <div className="testimonial-title-container">
             <h1 className="testimonial-title">Testimonials</h1>
           </div>
@@ -397,11 +443,15 @@ function Home() {
                     />
                   </div>
                   <br />
-                  <Typography variant="h6">{data.name}</Typography>
-                  <Typography variant="h6" color="textSecondary">
+                  <Typography className="card-testimonial-name">
+                    {data.name}
+                  </Typography>
+                  <Typography className="card-testimonial-title">
                     {data.title}
                   </Typography>
-                  <Typography color="textSecondary">{data.company}</Typography>
+                  <Typography className="card-testimonial-company">
+                    {data.company}
+                  </Typography>
                   <Typography
                     variant="body2"
                     component="p"
@@ -410,13 +460,26 @@ function Home() {
                     {data.description}
                   </Typography>
                   <br />
-                  <small>{data.date}</small>
+                  <small className="card-testimonial-date">{data.date}</small>
                 </CardContent>
               </Card>
             </Grid>
           );
         })}
-      </Grid> */}
+        <Grid item md={12} xs={12} className="testimonial-container">
+          <div className="testimonial-button-container">
+            <Button
+              variant="contained"
+              color="primary"
+              className="testimonial-button"
+              startIcon={<FaLinkedin />}
+              href="https://www.linkedin.com/in/judlup/"
+            >
+              See More
+            </Button>
+          </div>
+        </Grid>
+      </Grid>
     </div>
   );
 }
